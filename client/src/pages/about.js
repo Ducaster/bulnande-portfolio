0de59f35a2ece156bfe9eafabe7b6be9891e533b
nz <nz@nzui-MacBookPro-2.local>
2022-02-07 16:36:56 +0900

    어바웃페이지 레이아웃 완성 및 프로필 추가하기

--- client/src/pages/about.js
+++ client/src/pages/about.js
@@ -31,14 +31,13 @@ export const TextArea = styled.div`
     display: flex;
     justify-content: center;
     background-color: #5D728E;
-    // width: 1000px;
-    // height: 250px;
-    // font-size: 50px;
-    // position: relative;
     top: 50px;
     right: 500px;
 
     p {
+        position: relative;
+        top: 60px;
+        margin: 10px;
         font-size: 80px;
         color : white;
         text-align: center;
@@ -60,14 +59,14 @@ export const ValueList = styled.div`
 export const ValueImg = styled.div`
     display: flex;
     justify-content: center;
-    border: solid 1px black;
+    border: solid 1px red;
 
     .logo {
         width: 40px;
         height: 40px;
     }
     .title {
-        border: solid 1px black;
+        border: solid 1px red;
         display: flex;
         align-items: center;
         flex-direction: column;
@@ -77,11 +76,12 @@ export const ValueImg = styled.div`
 `;
 
 export const Musician = styled.div`
+    
     display: flex;
     justify-content: center;
     // align-items: center;
     border: solid 1px red;
-    margin: 20px;
+    margin: 50px;
     
     .musician_img {
         width: 500px;
@@ -97,17 +97,66 @@ export const Musician = styled.div`
         display: flex;
         flex-direction: column;
         border: solid 1px red;
+        text-align: center;
     }
 
     .portfolio_btn {
-        display: inline;
+        display: flex;
+        justify-content: center;
         border: solid 1px red;
         position: relative;
-        top: 200px;
-        left: 70px;
+        top: 300px;
+
     }
 
-`
+`;
+
+export const Concert = styled.div`
+    display: flex;
+    justify-content: center;
+    margin: 50px;
+    border: solid 1px red;
+
+    .concertInfo {
+        display: flex;
+        flex-direction: column;
+        border: solid 1px red;
+        text-align: center;
+        width: 500px;
+        margin: 10px;
+    }
+
+    h1 {
+        font-size: 30px;
+    }
+
+    .concertImg {
+        margin: 10px;
+        width: 500px;
+        display: flext;
+        // justify-content: center;
+        // align-items: row;
+        flex-direction: column;
+        border : solid 1px red;
+    }
+
+    .concertImg > div > div{
+        width: 250px;
+        height: 350px;
+        overflow: hidden;
+    }
+
+    .concertImg img {
+        width: 100%;
+        height: 100%;
+        transition: all 0.2s linear;
+        object-fit: cover;
+    }
+
+    .concertImg img:hover {
+        transform: scale(1.05);
+    }
+`;
 function About() {
     return (
         <>
@@ -115,7 +164,7 @@ function About() {
             <ImageTrans>
                 <img src="https://media.discordapp.net/attachments/938684956916449330/939025934282022962/51742564ceae459f.jpeg?width=701&height=468"></img>
                 <TextArea>
-                    <p>We support youth dream</p>
+                    <p>We support<br/>youth dream</p>
                 </TextArea>
             </ImageTrans>
             <ValueList>
@@ -154,7 +203,7 @@ function About() {
                 <div className='detail_comment'>
                     <div className='musician_comment'>
                         <h1>Value Creating for Artist</h1>
-                        <p>불난데 부채질은 아티스트들을 위한 무대를 제공합니다.<br/>뿐만 아니라 무명의 아티스트를 발굴하여 관객에게 소개합니다.</p>
+                        <p>we support stage for artist.<br/>And we discover unknown artists and introduce them to the audience.</p>
                     </div>
                     <div className='portfolioMove'>
                         <div className='portfolio_btn'>
@@ -165,6 +214,41 @@ function About() {
                     </div>
                 </div>
             </Musician>
+            <Concert>
+                <div className='concertInfo'>
+                    <h1>Passion Concert for youth</h1>
+                    <br/>
+                    <p>we support passion of youth.<br/>we want to explod your passion.</p>
+                </div>
+                <div className='concertImg'>
+                    <div>
+                        <div>
+                            <Link to='portfolio/detail/1'>
+                                <img name='하늘에별따기' src="https://cdn.discordapp.com/attachments/938684956916449330/939322806498394192/bulbuConcert.jpg"></img>
+                            </Link>
+                        </div>
+                        <div>
+                            <Link to='portfolio/detail/4'>
+                                <img name='1250project' src="https://media.discordapp.net/attachments/938684956916449330/939783195367444490/concert.2.jpeg"></img>
+                            </Link>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <Link to='portfolio/detail/3'>
+                                <img name='시즌'src="https://cdn.discordapp.com/attachments/938684956916449330/939783195673657364/concert1.jpeg"></img>
+                            </Link>
+                        </div>
+                        <div>
+                            <Link to='portfolio/detail/2'>
+                                <img name='소통' src="https://cdn.discordapp.com/attachments/938684956916449330/939783196034359296/concert3.jpeg"></img>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+                
+            </Concert>
+
             <Footer></Footer>
         </>
         
