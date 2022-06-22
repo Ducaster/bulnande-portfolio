e168787c63f0b41816e1543a77dd282bece03e79
outclass <mhlee@wemb.co.kr>
2022-06-22 10:19:52 +0900

    Feat: seed project, set project controller

--- server/router/projectRouter.js
+++ server/router/projectRouter.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/index");
 
-router.get("/", controller.portfolios.allPortfolios);
+router.get("/", controller.projects.allProjects);
+router.get("/:id", controller.projects.oneProject);
 
 module.exports = router;
